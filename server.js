bf67384bb0d2e4f6bca09e2820b736d6e009b0f3
cruzach <charliecruzan@gmail.com>
2018-10-17 22:41:10 -0400

    fixing cors

--- server.js
+++ server.js
@@ -17,11 +17,6 @@ app.use(cors())
 
 app.use(bodyParser.json());
 
-app.all('/signin', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
-  });
 
 app.get('/', (req, res)=> {
     res.send('this is working well');
