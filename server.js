9aa19265679cc15360f77bedb3c9cb690a6c2fc8
cruzach <charliecruzan@gmail.com>
2018-10-17 22:25:14 -0400

    fixing cors

--- server.js
+++ server.js
@@ -12,23 +12,16 @@ const db = knex({
     }
 });
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-        res.send(200);
-    } else {
-        next();
-    }
-};
 
 
 var app = express();
-app.use(allowCrossDomain);
-app.use(cors());
+
+const corsMiddleware = cors({
+    origin: [process.env.URL, 'http://localhost:4200']
+  })
+  
+  app.use(corsMiddleware)
+  app.options('*', corsMiddleware)
 
 app.use(bodyParser.json());
 
